544b5538e388a90ff2ac8abdeb31d11e9dfd4169
Ryan Shin <1982sdr@hanmail.net>
2017-03-27 11:26:46 +0900

    grade - manager app login added.

--- grade/settings.py
+++ grade/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'grade.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,5 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+LOGIN_REDIRECT_URL = '/post/'
 STATIC_URL = '/static/'
