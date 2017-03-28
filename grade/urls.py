186780598f1b44970dff9ae938fcf1f9563f7f32
Ryan Shin <1982sdr@hanmail.net>
2017-03-28 10:56:18 +0900

    grade - manager app register added.

--- grade/urls.py
+++ grade/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from manager.views import *
+from django.views.generic import TemplateView
+from manager.views import DuplicationCheck
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/accounts/login/',}, name='logout_url'),
+    url(r'^signup/$', signup, name='signup'),
+    url(r'^signup_ok/$',TemplateView.as_view(template_name='registration/signup_ok.html'), name='signup_ok'),
+    url(r'^duplcheck$', DuplicationCheck.as_view(), name='duplcheck'),
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+#회원가입
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, JsonResponse
+from django.core.urlresolvers import reverse
+from manager.forms import RegisterForm
+from django.views.generic import View
 
 # Create your views here.
+def signup(request):
+    """signsup
+    to register users
+    """
+    if request.method == "POST":
+        userform = RegisterForm(request.POST)
+        print(userform.is_valid())
+        if userform.is_valid():
+            userform.save()
+            return HttpResponseRedirect(
+                reverse("signup_ok")
+            )
+    elif request.method =="GET":
+        userform = RegisterForm()
+    return render(request, "registration/signup.html", {"userform": userform,})
+
+class DuplicationCheck(View):
+    def post(self, request):
+        # user = get_object_or_404(User, username=username)
+        username = request.POST.get('username', None)
+        data = {
+            'is_taken': User.objects.filter(username__iexact=username).exists()
+            }
+        return JsonResponse(data)
